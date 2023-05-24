9653001e864962a742a72786d0b7f07fc6fd6025
Ewerton <ewerton2004@yahoo.com.br>
2023-05-23 22:46:11 -0300

    feat(home): create the style of home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,46 @@
 'use client'
 import { useState } from 'react';
+import Player from '../../public/player.png'
+import Image from 'next/image';
 
 export default function LoginForm() {
   const [form, setForm] = useState('')
   const [username, setUsername] = useState('');
 
-  function handleLogin() {
+  function handleLogin(e) {
+    e.preventDefault();
 
     window.location.href =  '/main'; 
   }
 
-  function showForm(e) {
-    setForm('show-form')
-
-    e.target.remove()
-  }
-
   return (
-    <div>
-      <form className={`login-form ${form}`} onSubmit={handleLogin}>
-        <label>
-          Login: 
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-      <button type="submit">Fazer login</button>
-    </form>
-      <button id='show-login' type='button' onClick={showForm}>Login</button>
-      
-      <button>Sign In</button>
+    <div id='home'>
+      <Image 
+        width={750} 
+        height ={800} 
+        src={Player} 
+        alt='Jogador chutando uma bola'
+      />
+      <div id='login-div'>
+        <div>
+          <p>Olá</p>
+          <p>Bem Vindo ao ...!</p>
+        </div>
+        <p>Faça seu Login aqui</p>
+        <form className={`login-form ${form}`} onSubmit={handleLogin}>
+          <label htmlFor='login'>
+            <input
+              name='login'
+              id='login'
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <button type="submit">Entrar</button>
+        </form>
+        <p className='mid-text'>Ainda não tem sua KEY, <a href=''>CLIQUE AQUI</a></p>
+      </div>
     </div>
   );
 }
